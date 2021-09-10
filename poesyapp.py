94c2e40003c294b3a229241786d2c56f7ba0b1d5
sbtries <49175343+sbtries@users.noreply.github.com>
2021-09-10 19:48:52 -0400

    poesy attempt
    poesy is a lighter version of the same library, attempting to use to see if it provides easier to handle data

---
+++ poesyapp.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from poesy import Poem
+
+# class Poem():
+#     def __init__(self, title, text, author):
+#         self.title = title
+#         self.text = text
+#         self.author = author
+
+#     def parse(self):
+#         text = poesy.Text(self.text)
+#         text.parse() # must parse text before most methods are available
+#         stanzas = []
+#         for i, stanza in enumerate(text.children):
+#             i = {}
+#             for line in stanza.descendants():
+#                 line = prosodic.Text(str(line)) # make each line a text object to access text properties (like syllables) PROBS a better way, idk
+#                 line.parse()
+#                 i[str(line.bestParses())] = len(line.syllables()) #bestParse returns list of lines with caps/lower emphasis
+#             stanzas.append(i)
+#         return stanzas
+
+# create a flask object
+app = Flask(__name__)
+#config method that tells Flask where database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# initialize db
+db = SQLAlchemy(app)
+
+
+# define a url / routing: ex of template rendering
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        # try:
+            poem = Poem(text=request.form['poem'])
+            print('RESQUEST', request.form['poem'])
+            scansion = poem.schemed_beat
+            # return redirect('/')
+            return render_template('index.html', scansion=scansion, title=poem.title)
+        # except:
+        #     return 'There was an error parsing your text'
+    else:
+        scansion = []
+        return render_template('index.html', scansion=scansion)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/examples')
+def examples():
+    return render_template('examples.html')
+
+@app.route('/contact')
+def hello():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
